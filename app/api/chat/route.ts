e07bfafc3bf99806de83973b249d24882c8c46ec
Bohan Yang <youthdna@live.com>
2023-03-27 05:54:04 +0000

    Fix POST /api/chat

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,27 +1,23 @@
-import { OpenAIApi, Configuration } from "openai";
-import { ChatRequest } from "./typing";
-
 export async function POST(req: Request) {
   try {
     let apiKey = process.env.OPENAI_API_KEY;
+    let apiHost = process.env.OPENAI_API_HOST || 'https://api.openai.com';
 
     const userApiKey = req.headers.get("token");
     if (userApiKey) {
       apiKey = userApiKey;
     }
 
-    const openai = new OpenAIApi(
-      new Configuration({
-        apiKey,
-      })
-    );
-
-    const requestBody = (await req.json()) as ChatRequest;
-    const completion = await openai!.createChatCompletion({
-      ...requestBody,
+    const res = await fetch(`${apiHost}/v1/chat/completions`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      method: "POST",
+      body: req.body,
     });
 
-    return new Response(JSON.stringify(completion.data));
+    return new Response(res.body);
   } catch (e) {
     console.error("[Chat] ", e);
     return new Response(JSON.stringify(e));
