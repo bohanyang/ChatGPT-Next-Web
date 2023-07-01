a698fd20d8c0432e6e6a18f4f6e2d4607323f10d
Bohan Yang <youthdna@live.com>
2023-07-01 10:42:30 +0000

    Update

--- app/store/app.ts
+++ app/store/app.ts
@@ -76,15 +76,7 @@ export const ALL_MODELS = [
     available: ENABLE_GPT4,
   },
   {
-    name: "gpt-4-0314",
-    available: ENABLE_GPT4,
-  },
-  {
     name: "gpt-4-32k",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-4-32k-0314",
     available: ENABLE_GPT4,
   },
   {
@@ -92,7 +84,7 @@ export const ALL_MODELS = [
     available: true,
   },
   {
-    name: "gpt-3.5-turbo-0301",
+    name: "gpt-3.5-turbo-16k",
     available: true,
   },
 ];
