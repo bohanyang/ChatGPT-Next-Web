6c7cecc1b03435ef9bd7ec4600a052dbd5f9cae5
Bohan Yang <youthdna@live.com>
2023-07-23 13:07:29 +0000

    Update

--- app/constant.ts
+++ app/constant.ts
@@ -63,72 +63,21 @@ Knowledge cutoff: 2021-09
 Current model: {{model}}
 Current time: {{time}}`;
 
-const ENABLE_GPT4 = true;
-const ENABLE_GPT_MINOR = false;
-
 export const DEFAULT_MODELS = [
   {
     name: "gpt-4",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-4-0314",
-    available: ENABLE_GPT4 && ENABLE_GPT_MINOR,
-  },
-  {
-    name: "gpt-4-0613",
-    available: ENABLE_GPT4 && ENABLE_GPT_MINOR,
+    available: true,
   },
   {
     name: "gpt-4-32k",
-    available: ENABLE_GPT4,
-  },
-  {
-    name: "gpt-4-32k-0314",
-    available: ENABLE_GPT4 && ENABLE_GPT_MINOR,
-  },
-  {
-    name: "gpt-4-32k-0613",
-    available: ENABLE_GPT4 && ENABLE_GPT_MINOR,
+    available: true,
   },
   {
     name: "gpt-3.5-turbo",
     available: true,
   },
   {
-    name: "gpt-3.5-turbo-0301",
-    available: ENABLE_GPT_MINOR,
-  },
-  {
-    name: "gpt-3.5-turbo-0613",
-    available: ENABLE_GPT_MINOR,
-  },
-  {
     name: "gpt-3.5-turbo-16k",
     available: true,
-  },
-  {
-    name: "gpt-3.5-turbo-16k-0613",
-    available: ENABLE_GPT_MINOR,
-  },
-  {
-    name: "qwen-v1", // 通义千问
-    available: false,
-  },
-  {
-    name: "ernie", // 文心一言
-    available: false,
-  },
-  {
-    name: "spark", // 讯飞星火
-    available: false,
-  },
-  {
-    name: "llama", // llama
-    available: false,
-  },
-  {
-    name: "chatglm", // chatglm-6b
-    available: false,
   },
 ] as const;
